c786ea7425fd9d8eefc5dbff805c67dcca64b47a
Alex Rodrigues <alexrodriguesw2@hotmail.com>
2025-11-30 19:04:24 -0300

    first update

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const rent = 40;
+
+  if (days < 3) {
+    return rent * days;
+  }
+
+  if (days <= 6) {
+    return days * rent - 20;
+  }
+
+  if (days >= 7) {
+    return days * rent - 50;
+  }
 }
 
 module.exports = calculateRentalCost;
