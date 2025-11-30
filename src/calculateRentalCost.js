8fb40c641225f4d0140b21e1b0f6af8a59fb963b
Alex Rodrigues <alexrodriguesw2@hotmail.com>
2025-11-30 19:11:29 -0300

    second update

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,17 +5,19 @@
  */
 function calculateRentalCost(days) {
   const rent = 40;
+  const ShortsDiscount = 20;
+  const BigDiscount = 50;
 
   if (days < 3) {
     return rent * days;
   }
 
   if (days <= 6) {
-    return days * rent - 20;
+    return days * rent - ShortsDiscount;
   }
 
   if (days >= 7) {
-    return days * rent - 50;
+    return days * rent - BigDiscount;
   }
 }
 
